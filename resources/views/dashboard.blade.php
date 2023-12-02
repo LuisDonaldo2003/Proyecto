d69a862f1805688bcffe70d21f48b9e2f5c9baf7
LuisDonaldo2003 <l21930214@cdaltamirano.tecnm.mx>
2023-12-02 13:39:20 -0600

    Updated

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -3,21 +3,29 @@
 @section('title', 'Dashboard')
 
 @section('content_header')
-    <h1>Dashboard</h1>
+    <h1>Panel de Control de Inscripciones</h1>
 @stop
 
 @section('content')
-    <p>Bienvenido al Inicio.</p>
+    <p>Bienvenido al Panel de Control de Inscripciones. Aquí puedes gestionar todas las inscripciones de manera eficiente.</p>
 
     <!-- Botón para ir a la página de creación -->
-    <a href="{{ route('registrations.create') }}" class="btn btn-primary btn-lg">Crear Inscripción</a>
+    <a href="{{ route('registrations.create') }}" class="btn btn-primary btn-lg">
+        <i class="fas fa-plus-circle"></i> Crear Inscripción
+    </a>
 
     <!-- Botón para ir al CRUD (registrations.index) -->
-    <a href="{{ route('registrations.index') }}" class="btn btn-success btn-lg">Ver Inscripciones</a>
+    <a href="{{ route('registrations.index') }}" class="btn btn-success btn-lg">
+        <i class="fas fa-list"></i> Ver Inscripciones
+    </a>
+
+    <div class="mt-3">
+        <img src="{{ asset('assets/img/fondo.jpg') }}" alt="Logo de la empresa" class="img-fluid">
+    </div>
 @stop
 
 @section('css')
-    <link rel="stylesheet" href="/css/admin_custom.css">
+    <!-- No se utiliza CSS personalizado -->
 @stop
 
 @section('js')
